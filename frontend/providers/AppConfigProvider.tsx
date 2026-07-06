4fd8d54f401f1ef84c26e4f5fa2abe47bf03ac83
Alejandro <alejandro.marin92@eia.edu.co>
2026-07-06 12:17:22 -0500

    feat: crear AppConfigProvider con React Context y hook useAppConfig()

---
+++ frontend/providers/AppConfigProvider.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { createContext, useContext, useState, type ReactNode } from "react";
+
+interface AppConfig {
+  currency: string;
+  locale: string;
+  theme: string;
+}
+
+interface AppConfigContextType {
+  config: AppConfig;
+  updateConfig: (partial: Partial<AppConfig>) => void;
+}
+
+const defaultConfig: AppConfig = {
+  currency: "COP",
+  locale: "es-CO",
+  theme: "dark",
+};
+
+const AppConfigContext = createContext<AppConfigContextType>({
+  config: defaultConfig,
+  updateConfig: () => {},
+});
+
+export function AppConfigProvider({
+  children,
+  initialPreferences,
+}: {
+  children: ReactNode;
+  initialPreferences?: Partial<AppConfig>;
+}) {
+  const [config, setConfig] = useState<AppConfig>({
+    ...defaultConfig,
+    ...initialPreferences,
+  });
+
+  const updateConfig = (partial: Partial<AppConfig>) => {
+    setConfig((prev) => ({ ...prev, ...partial }));
+  };
+
+  return (
+    <AppConfigContext.Provider value={{ config, updateConfig }}>
+      {children}
+    </AppConfigContext.Provider>
+  );
+}
+
+export function useAppConfig() {
+  return useContext(AppConfigContext);
+}
